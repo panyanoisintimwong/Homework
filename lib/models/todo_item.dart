c265d05ac9ea17bd67e08bef14c035464033c96d
Panya Noisintimwong <noisintimwong_p@silpakorn.edu>
2023-10-18 23:22:45 +0700

    Initial commit

---
+++ lib/models/todo_item.dart
@@ -0,0 +1,28 @@
+/*
+{
+  userId: 1,
+  id: 1,
+  title: "delectus aut autem",
+  completed: false
+}
+*/
+
+class TodoItem {
+  final int userId;
+  final int id;
+  final String title;
+
+  TodoItem({
+    required this.userId,
+    required this.id,
+    required this.title,
+  });
+
+  factory TodoItem.fromJson(Map<String, dynamic> json) {
+    return TodoItem(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+    );
+  }
+}
---
+++ lib/screens/home/home_page.dart
@@ -0,0 +1,85 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:todo_list/models/todo_item.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _dio = Dio(BaseOptions(responseType: ResponseType.plain));
+  List<TodoItem>? _itemList;
+  String? _error;
+
+  void getTodos() async {
+    try {
+      setState(() {
+        _error = null;
+      });
+
+      // await Future.delayed(const Duration(seconds: 3), () {});
+
+      final response =
+      await _dio.get('https://jsonplaceholder.typicode.com/albums');
+      debugPrint(response.data.toString());
+      // parse
+      List list = jsonDecode(response.data.toString());
+      setState(() {
+        _itemList = list.map((item) => TodoItem.fromJson(item)).toList();
+      });
+    } catch (e) {
+      setState(() {
+        _error = e.toString();
+      });
+      debugPrint('เกิดข้อผิดพลาด: ${e.toString()}');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getTodos();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget body;
+
+    if (_error != null) {
+      body = Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(_error!),
+          const SizedBox(height: 16),
+          ElevatedButton(
+            onPressed: () {
+              getTodos();
+            },
+            child: const Text('RETRY'),
+          )
+        ],
+      );
+    } else if (_itemList == null) {
+      body = const Center(child: CircularProgressIndicator());
+    } else {
+      body = ListView.builder(
+          itemCount: _itemList!.length,
+          itemBuilder: (context, index) {
+            var todoItem = _itemList![index];
+            return Card(
+                child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Row(children: [
+                      Expanded(child: Text(todoItem.title)),
+                    ])));
+          });
+    }
+
+    return Scaffold(body: body);
+  }
+}
